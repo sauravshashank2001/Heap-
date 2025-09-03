6175a33bdbec9affd8c9506cc95b182c31d2fd4f
shashank <sauravshashank2001@gmail.com>
2025-09-03 23:24:34 +0530

    longest Repeating Char Replacememt

---
+++ Algorithm/TwoPointers&slidingWindow/LongestRepeatngChar.java
@@ -0,0 +1,40 @@
+/**
+ * You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. You can perform this operation at most k times.
+
+Return the length of the longest substring containing the same letter you can get after performing the above operations.
+
+ 
+
+Example 1:
+
+Input: s = "ABAB", k = 2
+Output: 4
+Explanation: Replace the two 'A's with two 'B's or vice versa.
+ * 
+ */
+
+
+import java.util.HashMap;
+
+class Solution {
+    public int characterReplacement(String s, int k) {
+        HashMap<Character, Integer> freqs = new HashMap<>();
+        int res = 0, i = 0, maxFreq = 0;
+
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            freqs.put(c, freqs.getOrDefault(c, 0) + 1);
+            maxFreq = Math.max(maxFreq, freqs.get(c));
+
+            while ((j - i + 1) - maxFreq > k) {
+                char left = s.charAt(i);
+                freqs.put(left, freqs.get(left) - 1);
+                i++;
+            }
+
+            res = Math.max(res, j - i + 1);
+        }
+
+        return res;
+    }
+}
