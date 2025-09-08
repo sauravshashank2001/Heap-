d92f21802a608ebf95845880cc6d2b258402b358
shashank <sauravshashank2001@gmail.com>
2025-09-08 22:57:14 +0530

    Binary subarray with sum

---
+++ Algorithm/TwoPointers&slidingWindow/BinarySubArrayWithSum.java
@@ -0,0 +1,46 @@
+/**
+ * 930. Binary Subarrays With Sum
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
+
+A subarray is a contiguous part of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,1,0,1], goal = 2
+Output: 4
+Explanation: The 4 subarrays are bolded and underlined below:
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+[1,0,1,0,1]
+
+ * 
+ * 
+ * 
+ */
+
+
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+    private int atMost(int[] nums, int goal) {
+        int head, tail = 0, sum = 0, result = 0;
+        for (head = 0; head < nums.length; head++) {
+            sum += nums[head];
+            while (sum > goal && tail <= head) {
+                sum -= nums[tail];
+                tail++;
+            }
+            result += head - tail + 1;
+        }
+        return result;
+    }
+}
